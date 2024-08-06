2afd7970cdd0e6c3d311ed41a0126ff71383619c
Edgar Abel Espino Alba <edgarabelespinolba@gmail.com>
2024-08-06 11:53:32 -0600

    Initial Commit

---
+++ testiOs/testiOsApp.swift
@@ -0,0 +1,17 @@
+//
+//  testiOsApp.swift
+//  testiOs
+//
+//  Created by Edgar Abel Espino Alba on 06/08/24.
+//
+
+import SwiftUI
+
+@main
+struct testiOsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
